fed43a6cdd0b5259d34c3dbaa89f72e9075ef9b4
Wojciech Witon <wojtekwiton@gmail.com>
2016-11-14 16:41:41 +0100

    Changes in matlab files

--- code/main.m
+++ code/main.m
@@ -1,4 +1,14 @@
-function importData(fileName)
-    
+fileList = dir('../doc/data');
+fileList = fileList(3:end);
+fileListsize = size(fileList,1);
+
+filePaths = strings(fileListsize,1);
+
+for i=1:fileListsize
+   filePaths(i) = strcat(fileList(i).folder, '/', fileList(i).name);
 end
 
+delimiter = ';';
+headers = 1;
+%A = dlmread(filePaths(1),delimiter);
+A = importdata(filePaths(1), delimiter, headers);
