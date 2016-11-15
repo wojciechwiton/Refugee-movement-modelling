886cd67d0abcf92eab1b5489bc75e4c22eb8b8a6
Wojciech Witon <wojtekwiton@gmail.com>
2016-11-15 11:51:04 +0100

    Name changes of csv datasets

--- code/main.m
+++ code/main.m
@@ -5,10 +5,13 @@ fileListsize = size(fileList,1);
 filePaths = strings(fileListsize,1);
 
 for i=1:fileListsize
-   filePaths(i) = strcat(fileList(i).folder, '/', fileList(i).name);
+   filePaths(i) = fullfile(fileList(i).folder, fileList(i).name);
 end
 
 delimiter = ';';
 headers = 1;
 %A = dlmread(filePaths(1),delimiter);
-A = importdata(filePaths(1), delimiter, headers);
+% A = importdata(filePaths(1), delimiter, headers);
+% A = uiimport(filePaths(1));
+fileID = fopen(filePaths(1));
+A = textscan(fileID, '%s%s%s%s%s%s');
